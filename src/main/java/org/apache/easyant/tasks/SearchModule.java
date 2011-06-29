7debe5e5553d489de510795feec2dbd401122ee4
Jean-Louis Boudart <jlboudart@apache.org>
2011-06-29 13:18:24 +0000

    Avoid NPE on SearchModule task when errors appears when resolved metadata are not in cache
    git-svn-id: https://svn.apache.org/repos/asf/incubator/easyant/core/trunk@1141090 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/easyant/tasks/SearchModule.java
+++ src/main/java/org/apache/easyant/tasks/SearchModule.java
@@ -28,170 +28,179 @@ import org.apache.ivy.core.settings.IvySettings;
 import org.apache.ivy.plugins.matcher.PatternMatcher;
 import org.apache.ivy.plugins.resolver.DependencyResolver;
 import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Project;
 import org.apache.tools.ant.input.InputHandler;
 import org.apache.tools.ant.input.InputRequest;
 import org.apache.tools.ant.input.MultipleChoiceInputRequest;
 
 public class SearchModule extends IvyTask {
 
-    private String organisation;
+	private String organisation;
 
-    private String module;
+	private String module;
 
-    private String branch = PatternMatcher.ANY_EXPRESSION;
+	private String branch = PatternMatcher.ANY_EXPRESSION;
 
-    private String revision;
+	private String revision;
 
-    private String matcher = PatternMatcher.EXACT_OR_REGEXP;
+	private String matcher = PatternMatcher.EXACT_OR_REGEXP;
 
-    private String propertyPrefix;
-    private String resolver;
+	private String propertyPrefix;
+	private String resolver;
 
-    public void doExecute() throws BuildException {
-        if (getOrganisation() == null) {
-            throw new BuildException(
-                    "no organisation provided for SearchModule task");
-        }
-        if (getModule() == null) {
-            throw new BuildException(
-                    "no module name provided for SearchModule task");
-        }
-        if (getRevision() == null) {
-            throw new BuildException(
-                    "no revision provided for SearchModule task");
-        }
+	public void doExecute() throws BuildException {
+		if (getOrganisation() == null) {
+			throw new BuildException(
+					"no organisation provided for SearchModule task");
+		}
+		if (getModule() == null) {
+			throw new BuildException(
+					"no module name provided for SearchModule task");
+		}
+		if (getRevision() == null) {
+			throw new BuildException(
+					"no revision provided for SearchModule task");
+		}
 
-        if (getPropertyPrefix() == null) {
-            throw new BuildException(
-                    "no property prefix provided provided for SearchModule task");
-        }
-        Ivy ivy = getIvyInstance();
-        IvySettings settings = ivy.getSettings();
+		if (getPropertyPrefix() == null) {
+			throw new BuildException(
+					"no property prefix provided provided for SearchModule task");
+		}
+		Ivy ivy = getIvyInstance();
+		IvySettings settings = ivy.getSettings();
 
-        // search all modules revision matching the requested criteria
-        DependencyResolver resolverToCheck;
-        if (getResolver() != null) {
-            resolverToCheck = settings.getResolver(getResolver());
-        } else {
-            resolverToCheck = settings.getDefaultResolver();
-        }
-        ModuleRevisionId mridToSearch = ModuleRevisionId.newInstance(
-                getOrganisation(), getModule(), getBranch(), getRevision());
-        ModuleRevisionId[] mrids = ivy.getSearchEngine().listModules(
-                resolverToCheck, mridToSearch, settings.getMatcher(matcher));
+		// search all modules revision matching the requested criteria
+		DependencyResolver resolverToCheck;
+		if (getResolver() != null) {
+			resolverToCheck = settings.getResolver(getResolver());
+		} else {
+			resolverToCheck = settings.getDefaultResolver();
+		}
+		ModuleRevisionId mridToSearch = ModuleRevisionId.newInstance(
+				getOrganisation(), getModule(), getBranch(), getRevision());
+		ModuleRevisionId[] mrids = ivy.getSearchEngine().listModules(
+				resolverToCheck, mridToSearch, settings.getMatcher(matcher));
 
-        // diplay the list
-        Vector<String> choices = new Vector<String>();
-        for (int i = 0; i < mrids.length; i++) {
-            choices.add(String.valueOf(i));
-            StringBuilder sb = new StringBuilder();
-            sb.append(i).append(": ");
-            sb.append(mrids[i].getName());
-            sb.append(" v").append(mrids[i].getRevision());
-            // hide organization if its the default one
-            if (!EasyAntConstants.EASYANT_SKELETONS_ORGANISATION
-                    .equals(mrids[i].getOrganisation())) {
-                sb.append(" by ").append(mrids[i].getOrganisation());
-            }
-            // Get the description
-            ResolvedModuleRevision rmr = ivy.findModule(mrids[i]);
-            if (rmr.getDescriptor().getDescription() != null
-                    && !rmr.getDescriptor().getDescription().equals("")) {
-                sb.append(" (").append(rmr.getDescriptor().getDescription())
-                        .append(")");
-            }
-            log(sb.toString());
+		// diplay the list
+		Vector<String> choices = new Vector<String>();
+		for (int i = 0; i < mrids.length; i++) {
+			ResolvedModuleRevision rmr = ivy.findModule(mrids[i]);
+			if (rmr == null) {
+				log("Can't retrieve " + mrids[i].toString(),Project.MSG_WARN);
+			} else {
 
-        }
+				choices.add(String.valueOf(i));
+				StringBuilder sb = new StringBuilder();
+				sb.append(i).append(": ");
+				sb.append(mrids[i].getName());
+				sb.append(" v").append(mrids[i].getRevision());
+				// hide organization if its the default one
+				if (!EasyAntConstants.EASYANT_SKELETONS_ORGANISATION
+						.equals(mrids[i].getOrganisation())) {
+					sb.append(" by ").append(mrids[i].getOrganisation());
+				}
+				// Get the description
+				if (rmr.getDescriptor().getDescription() != null
+						&& !rmr.getDescriptor().getDescription().equals("")) {
+					sb.append(" (")
+							.append(rmr.getDescriptor().getDescription())
+							.append(")");
+				}
+				log(sb.toString());
+			}
+		}
+		if (choices.size() == 0) {
+			throw new BuildException("No matching module were found !");
+		}
 
-        // ask end user to select a module
-        // TODO handle a default value
-        Integer value = new Integer(getInput("Choose a number:", null, choices));
-        ModuleRevisionId moduleToRetrieve = mrids[value];
+		// ask end user to select a module
+		// TODO handle a default value
+		Integer value = new Integer(getInput("Choose a number:", null, choices));
+		ModuleRevisionId moduleToRetrieve = mrids[value];
 
-        // set final properties
-        getProject().setProperty(getPropertyPrefix() + ".org",
-                moduleToRetrieve.getOrganisation());
-        getProject().setProperty(getPropertyPrefix() + ".module",
-                moduleToRetrieve.getName());
-        getProject().setProperty(getPropertyPrefix() + ".rev",
-                moduleToRetrieve.getRevision());
+		// set final properties
+		getProject().setProperty(getPropertyPrefix() + ".org",
+				moduleToRetrieve.getOrganisation());
+		getProject().setProperty(getPropertyPrefix() + ".module",
+				moduleToRetrieve.getName());
+		getProject().setProperty(getPropertyPrefix() + ".rev",
+				moduleToRetrieve.getRevision());
 
-    }
+	}
 
-    protected String getInput(String message, String defaultvalue,
-            Vector<String> choices) {
-        InputRequest request = null;
-        request = new MultipleChoiceInputRequest(message, choices);
-        request.setDefaultValue(defaultvalue);
+	protected String getInput(String message, String defaultvalue,
+			Vector<String> choices) {
+		InputRequest request = null;
+		request = new MultipleChoiceInputRequest(message, choices);
+		request.setDefaultValue(defaultvalue);
 
-        InputHandler h = getProject().getInputHandler();
+		InputHandler h = getProject().getInputHandler();
 
-        h.handleInput(request);
+		h.handleInput(request);
 
-        String value = request.getInput();
-        if ((value == null || value.trim().length() == 0)
-                && defaultvalue != null) {
-            value = defaultvalue;
-        }
-        return value;
+		String value = request.getInput();
+		if ((value == null || value.trim().length() == 0)
+				&& defaultvalue != null) {
+			value = defaultvalue;
+		}
+		return value;
 
-    }
+	}
 
-    public String getMatcher() {
-        return matcher;
-    }
+	public String getMatcher() {
+		return matcher;
+	}
 
-    public void setMatcher(String matcher) {
-        this.matcher = matcher;
-    }
+	public void setMatcher(String matcher) {
+		this.matcher = matcher;
+	}
 
-    public String getModule() {
-        return module;
-    }
+	public String getModule() {
+		return module;
+	}
 
-    public void setModule(String module) {
-        this.module = module;
-    }
+	public void setModule(String module) {
+		this.module = module;
+	}
 
-    public String getOrganisation() {
-        return organisation;
-    }
+	public String getOrganisation() {
+		return organisation;
+	}
 
-    public void setOrganisation(String organisation) {
-        this.organisation = organisation;
-    }
+	public void setOrganisation(String organisation) {
+		this.organisation = organisation;
+	}
 
-    public String getRevision() {
-        return revision;
-    }
+	public String getRevision() {
+		return revision;
+	}
 
-    public void setRevision(String revision) {
-        this.revision = revision;
-    }
+	public void setRevision(String revision) {
+		this.revision = revision;
+	}
 
-    public String getBranch() {
-        return branch;
-    }
+	public String getBranch() {
+		return branch;
+	}
 
-    public void setBranch(String branch) {
-        this.branch = branch;
-    }
+	public void setBranch(String branch) {
+		this.branch = branch;
+	}
 
-    public String getPropertyPrefix() {
-        return propertyPrefix;
-    }
+	public String getPropertyPrefix() {
+		return propertyPrefix;
+	}
 
-    public void setPropertyPrefix(String propertyPrefix) {
-        this.propertyPrefix = propertyPrefix;
-    }
+	public void setPropertyPrefix(String propertyPrefix) {
+		this.propertyPrefix = propertyPrefix;
+	}
 
-    public String getResolver() {
-        return resolver;
-    }
+	public String getResolver() {
+		return resolver;
+	}
 
-    public void setResolver(String resolver) {
-        this.resolver = resolver;
-    }
+	public void setResolver(String resolver) {
+		this.resolver = resolver;
+	}
 
 }
