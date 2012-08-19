f62240a25fce220e0d506f44723cb039ba66c722
Jean-Louis Boudart <jlboudart@apache.org>
2012-08-19 20:38:05 +0000

    EASYANT-47 : listProps switch should display all properties if no argument is specified
    git-svn-id: https://svn.apache.org/repos/asf/incubator/easyant/core/trunk@1374843 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/easyant/man/ListProps.java
+++ src/main/java/org/apache/easyant/man/ListProps.java
@@ -25,52 +25,68 @@ import org.apache.easyant.core.descriptor.PropertyDescriptor;
 import org.apache.easyant.core.report.ImportedModuleReport;
 
 /**
- * Lists all properties (deep search - includes all imported modules)
- * available in the specified build module.
+ * Lists all properties (deep search - includes all imported modules) available in the specified build module.
  */
 public class ListProps extends EasyantOption {
-    
-    public ListProps()
-            throws IllegalArgumentException {
+
+    public ListProps() throws IllegalArgumentException {
         super("listProps", false, "List all properties available or specified in a given plugin as argument");
         setOptionalArg(true);
         setStopBuild(true);
     }
 
     public void execute() {
+        getProject().log(
+                LINE_SEP + "--- Available Properties for current project: " + getProject().getName() + " ---" + LINE_SEP);
         /*
-         * the plugin specified to this class through the addParam method
-         * needs to be searched for all properties, and those
-         * properties will be displayed by this class.
+         * the plugin specified to this class through the addParam method needs to be searched for all properties, and
+         * those properties will be displayed by this class.
          */
         String plugin = getValue();
-        if(plugin == null || plugin.trim().length() == 0) {
-            throw new IllegalArgumentException("No plugin found to list properties for.");
-        }
-
-        getProject().log(LINE_SEP+ "--- Available Properties for current project: " + getProject().getName() + 
-                ":: Plugin: " + plugin + " ---" + LINE_SEP);
-
-        ImportedModuleReport moduleRep = getEareport().getImportedModuleReport(plugin);
-        if(moduleRep == null) {
-            getProject().log("\tNo Module / Plugin found by given name: " + plugin);
-        } else {
-            Map<String, PropertyDescriptor> allprops = moduleRep.getEasyantReport().getPropertyDescriptors();
+        if (plugin == null || plugin.trim().length() == 0) {
             
-            if(allprops.size() > 0) {
-                for(Iterator<Entry<String, PropertyDescriptor>> it = allprops.entrySet().iterator(); it.hasNext(); ) {
-                    Entry<String, PropertyDescriptor> entry = it.next();
-                    PropertyDescriptor prop = entry.getValue();
-                    getProject().log("\tProperty: " + prop.getName());
-                }
-        
-                getProject().log(LINE_SEP+LINE_SEP+ "For more information on a Property, run:" + LINE_SEP 
-                        + "\t easyant -describe <PROPERTY>");
+            getProject().log(
+                    LINE_SEP + "No plugin specified. Listing all properties available in the project.");
+            
+            Map<String, PropertyDescriptor> allProjectProps =getEareport().getPropertyDescriptors();
+            if (allProjectProps.size() > 0) {
+                printProperties(allProjectProps);
+
             } else {
-                getProject().log(LINE_SEP + "No property found in the plugin: " + plugin);
+                getProject().log(LINE_SEP + "No property found in current project");
+            }
+            
+        } else {
+            getProject().log(
+                    LINE_SEP + "--- Filtering properties declared in the following plugin: "
+                            + plugin + " ---" + LINE_SEP);
+            ImportedModuleReport moduleRep = getEareport().getImportedModuleReport(plugin);
+            if (moduleRep == null) {
+                getProject().log("\tNo Module / Plugin found by given name: " + plugin);
+            } else {
+                Map<String, PropertyDescriptor> allprops = moduleRep.getEasyantReport().getPropertyDescriptors();
+
+                if (allprops.size() > 0) {
+                    printProperties(allprops);
+
+                    getProject().log(
+                            LINE_SEP + LINE_SEP + "For more information on a Property, run:" + LINE_SEP
+                                    + "\t easyant -describe <PROPERTY>");
+                } else {
+                    getProject().log(LINE_SEP + "No property found in the plugin: " + plugin);
+                }
             }
         }
         getProject().log(LINE_SEP + "--- End Of (Properties Listing) ---");
+    }
+
+    private void printProperties(Map<String, PropertyDescriptor> allProjectProps) {
+        for (Iterator<Entry<String, PropertyDescriptor>> it = allProjectProps.entrySet().iterator(); it.hasNext();) {
+            Entry<String, PropertyDescriptor> entry = it.next();
+            PropertyDescriptor prop = entry.getValue();
+            getProject().log("\tProperty: " + prop.getName() );
+            
+        }
     }
 
 }
