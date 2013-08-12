9ad4fe313a9386872966d90ac127de57b289a1a6
Jean-Louis Boudart <jlboudart@apache.org>
2013-08-12 20:26:42 +0000

    Add tests on Import and ResolvePluginsTest
    git-svn-id: https://svn.apache.org/repos/asf/ant/easyant/core/trunk@1513245 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/java/org/apache/easyant/tasks/ImportTest.java
@@ -0,0 +1,273 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+package org.apache.easyant.tasks;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+
+import junit.framework.Assert;
+
+import org.apache.easyant.core.EasyAntMagicNames;
+import org.apache.easyant.core.ant.ProjectUtils;
+import org.apache.ivy.ant.IvyConfigure;
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Location;
+import org.apache.tools.ant.Project;
+import org.apache.tools.ant.taskdefs.Delete;
+import org.apache.tools.ant.types.Path;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ImportTest {
+    private File cache;
+
+    private Import importTask;
+
+    @Before
+    public void setUp() throws MalformedURLException, URISyntaxException {
+        createCache();
+        Project project = new Project();
+        ProjectUtils.configureProjectHelper(project);
+        project.setProperty("ivy.cache.dir", cache.getAbsolutePath());
+
+        IvyConfigure configure = new IvyConfigure();
+        configure.setProject(project);
+
+        File f = new File(this.getClass().getResource("/repositories/easyant-ivysettings-test.xml").toURI());
+        configure.setFile(f);
+
+        configure.setSettingsId(EasyAntMagicNames.EASYANT_IVY_INSTANCE);
+        configure.execute();
+
+        importTask = new Import();
+        importTask.setProject(project);
+        importTask.setOwningTarget(ProjectUtils.createTopLevelTarget());
+        importTask.setLocation(new Location(ProjectUtils.emulateMainScript(project).getAbsolutePath()));
+    }
+
+    private void createCache() {
+        cache = new File("build/cache");
+        cache.mkdirs();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        cleanCache();
+    }
+
+    private void cleanCache() {
+        Delete del = new Delete();
+        del.setProject(new Project());
+        del.setDir(cache);
+        del.execute();
+    }
+
+    @Test(expected = BuildException.class)
+    public void shouldFailIfNoMandatoryAttributeAreSet() {
+        importTask.execute();
+    }
+
+    @Test
+    public void shouldIncludeSimplePlugin() {
+        importTask.setOrg("mycompany");
+        importTask.setModule("simpleplugin");
+        importTask.setRev("0.1");
+        importTask.setMode("include");
+        importTask.execute();
+
+        Path pluginClasspath = importTask.getProject().getReference("mycompany#simpleplugin.classpath");
+        Assert.assertNotNull(pluginClasspath);
+        Assert.assertEquals(0, pluginClasspath.list().length);
+    }
+
+    @Test
+    public void shouldComputeAsAttributeOnIncludeWithMRID() {
+        importTask.setMrid("mycompany#simpleplugin;0.1");
+        importTask.setMode("include");
+        importTask.execute();
+
+        // as attribute is preconfigured with module name
+        Assert.assertNotNull(importTask.getAs());
+        Assert.assertEquals("simpleplugin", importTask.getAs());
+    }
+
+    @Test
+    public void shouldImportSimplePlugin() {
+        importTask.setOrg("mycompany");
+        importTask.setModule("simpleplugin");
+        importTask.setRev("0.1");
+        importTask.execute();
+
+        Path pluginClasspath = importTask.getProject().getReference("mycompany#simpleplugin.classpath");
+        Assert.assertNotNull(pluginClasspath);
+        Assert.assertEquals(0, pluginClasspath.list().length);
+    }
+
+    @Test
+    public void shouldImportSimplePluginWithProperties() {
+        importTask.setOrg("mycompany");
+        importTask.setModule("simplepluginwithproperties");
+        importTask.setRev("0.1");
+        importTask.execute();
+
+        verifySimplePluginWithPropertiesIsImported();
+    }
+
+    private void verifySimplePluginWithPropertiesIsImported() {
+        Path pluginClasspath = importTask.getProject().getReference("mycompany#simplepluginwithproperties.classpath");
+        Assert.assertNotNull(pluginClasspath);
+        Assert.assertEquals(0, pluginClasspath.list().length);
+
+        String propertiesFileLocation = importTask.getProject().getProperty(
+                "mycompany#simplepluginwithproperties.properties.file");
+        Assert.assertNotNull(propertiesFileLocation);
+
+        String srcMainJavaProperty = importTask.getProject().getProperty("src.main.java");
+        Assert.assertNotNull(srcMainJavaProperty);
+        Assert.assertEquals(importTask.getProject().getBaseDir() + "/src/main/java", srcMainJavaProperty);
+
+        // imported from property file
+        String aProperty = importTask.getProject().getProperty("aproperty");
+        Assert.assertNotNull(aProperty);
+        Assert.assertEquals("value", aProperty);
+
+        String anotherJavaProperty = importTask.getProject().getProperty("anotherproperty");
+        Assert.assertNotNull(anotherJavaProperty);
+        Assert.assertEquals("value", anotherJavaProperty);
+    }
+
+    @Test
+    public void shouldSkipSimplePlugin() {
+        importTask.getProject().setNewProperty("skip.mycompany#simplepluginwithproperties;0.1", "true");
+        importTask.setOrg("mycompany");
+        importTask.setModule("simplepluginwithproperties");
+        importTask.setRev("0.1");
+        importTask.execute();
+
+        verifySimplePluginWithPropertiesIsSkipped();
+
+    }
+
+    private void verifySimplePluginWithPropertiesIsSkipped() {
+        Path pluginClasspath = importTask.getProject().getReference("mycompany#simplepluginwithproperties.classpath");
+        Assert.assertNull(pluginClasspath);
+
+        String propertiesFileLocation = importTask.getProject().getProperty(
+                "mycompany#simplepluginwithproperties.properties.file");
+        Assert.assertNull(propertiesFileLocation);
+
+        String srcMainJavaProperty = importTask.getProject().getProperty("src.main.java");
+        Assert.assertNull(srcMainJavaProperty);
+
+        // imported from property file
+        String aProperty = importTask.getProject().getProperty("aproperty");
+        Assert.assertNull(aProperty);
+
+        String anotherJavaProperty = importTask.getProject().getProperty("anotherproperty");
+        Assert.assertNull(anotherJavaProperty);
+    }
+
+    @Test
+    public void shouldSkipSimplePluginWithAsAttribute() {
+        importTask.getProject().setNewProperty("skip.myalias", "true");
+        importTask.setOrg("mycompany");
+        importTask.setModule("simplepluginwithproperties");
+        importTask.setRev("0.1");
+        importTask.setAs("myalias");
+        importTask.execute();
+
+        verifySimplePluginWithPropertiesIsSkipped();
+
+    }
+
+    @Test
+    public void shouldNotSkipMandatoryPlugin() {
+        importTask.getProject().setNewProperty("skip.mycompany#simplepluginwithproperties;0.1", "true");
+        importTask.setOrg("mycompany");
+        importTask.setModule("simplepluginwithproperties");
+        importTask.setRev("0.1");
+        importTask.setMandatory(true);
+        importTask.execute();
+
+        verifySimplePluginWithPropertiesIsImported();
+    }
+
+    @Test
+    public void shouldImportMandatoryPlugin() {
+        importTask.setOrg("mycompany");
+        importTask.setModule("simplepluginwithproperties");
+        importTask.setRev("0.1");
+        importTask.setMandatory(true);
+        importTask.execute();
+
+        verifySimplePluginWithPropertiesIsImported();
+    }
+
+    @Test
+    public void shouldComputeAsAttributeOnInclude() {
+        importTask.setOrg("mycompany");
+        importTask.setModule("simpleplugin");
+        importTask.setRev("0.1");
+        importTask.setMode("include");
+        importTask.execute();
+
+        // as attribute is preconfigured with module name
+        Assert.assertNotNull(importTask.getAs());
+        Assert.assertEquals("simpleplugin", importTask.getAs());
+    }
+
+    @Test(expected = BuildException.class)
+    public void shouldFailBuildConfAreFound() {
+        importTask.setOrg("mycompany");
+        importTask.setModule("simplepluginwithproperties");
+        importTask.setRev("0.1");
+        importTask.setBuildConfigurations("amissingConf");
+        importTask.execute();
+        //
+    }
+
+    @Test
+    public void shouldNotImportIfBuildConfDoesntMatch() {
+        importTask.getProject().setProperty(EasyAntMagicNames.AVAILABLE_BUILD_CONFIGURATIONS, "aBuildConfNotActive");
+        importTask.setOrg("mycompany");
+        importTask.setModule("simplepluginwithproperties");
+        importTask.setRev("0.1");
+        importTask.setBuildConfigurations("aBuildConfNotActive");
+        importTask.execute();
+
+        verifySimplePluginWithPropertiesIsSkipped();
+    }
+
+    @Test
+    public void shouldImportIfBuildConfMatch() {
+        importTask.getProject().setProperty(EasyAntMagicNames.AVAILABLE_BUILD_CONFIGURATIONS, "aBuildConfActive");
+
+        importTask.getProject().setProperty(EasyAntMagicNames.MAIN_CONFS, "aBuildConfActive");
+
+        importTask.setOrg("mycompany");
+        importTask.setModule("simplepluginwithproperties");
+        importTask.setRev("0.1");
+        importTask.setBuildConfigurations("aBuildConfActive");
+        importTask.execute();
+
+        verifySimplePluginWithPropertiesIsImported();
+    }
+
+}
---
+++ src/test/java/org/apache/easyant/tasks/ResolvePluginsTest.java
@@ -0,0 +1,116 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+package org.apache.easyant.tasks;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+
+import junit.framework.Assert;
+
+import org.apache.easyant.core.EasyAntMagicNames;
+import org.apache.easyant.tasks.ResolvePlugins;
+import org.apache.ivy.ant.IvyConfigure;
+import org.apache.ivy.ant.IvyDependency;
+import org.apache.ivy.core.report.ResolveReport;
+import org.apache.tools.ant.Project;
+import org.apache.tools.ant.taskdefs.Delete;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ResolvePluginsTest {
+    private File cache;
+
+    private ResolvePlugins resolvePlugins;
+
+    @Before
+    public void setUp() throws MalformedURLException, URISyntaxException {
+        createCache();
+        Project project = new Project();
+
+        project.setProperty("ivy.cache.dir", cache.getAbsolutePath());
+
+        IvyConfigure configure = new IvyConfigure();
+        configure.setProject(project);
+
+        File f = new File(this.getClass().getResource("/repositories/easyant-ivysettings-test.xml").toURI());
+        configure.setFile(f);
+
+        configure.setSettingsId(EasyAntMagicNames.EASYANT_IVY_INSTANCE);
+        configure.execute();
+
+        resolvePlugins = new ResolvePlugins();
+        resolvePlugins.setProject(project);
+    }
+
+    private void createCache() {
+        cache = new File("build/cache");
+        cache.mkdirs();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        cleanCache();
+    }
+
+    private void cleanCache() {
+        Delete del = new Delete();
+        del.setProject(new Project());
+        del.setDir(cache);
+        del.execute();
+    }
+
+    @Test
+    public void shouldCreateEmptyResolveReport() {
+        resolvePlugins.execute();
+        ResolveReport report = resolvePlugins.getProject().getReference(
+                EasyAntMagicNames.IMPORTED_MODULES_RESOLVE_REPORT_REF);
+        Assert.assertNotNull(report);
+        Assert.assertEquals(0, report.getDependencies().size());
+    }
+
+    @Test
+    public void shouldContainUnresolvedDependency() {
+        IvyDependency dependency = resolvePlugins.createDependency();
+        dependency.setOrg("does");
+        dependency.setName("not");
+        dependency.setRev("exist");
+        resolvePlugins.execute();
+        ResolveReport report = resolvePlugins.getProject().getReference(
+                EasyAntMagicNames.IMPORTED_MODULES_RESOLVE_REPORT_REF);
+        Assert.assertNotNull(report);
+        Assert.assertEquals(1, report.getDependencies().size());
+        Assert.assertEquals(1, report.getUnresolvedDependencies().length);
+    }
+
+    @Test
+    public void shouldContainResolvedDependency() {
+        IvyDependency dependency = resolvePlugins.createDependency();
+        dependency.setOrg("mycompany");
+        dependency.setName("simpleplugin");
+        dependency.setRev("0.1");
+        resolvePlugins.execute();
+        ResolveReport report = resolvePlugins.getProject().getReference(
+                EasyAntMagicNames.IMPORTED_MODULES_RESOLVE_REPORT_REF);
+        Assert.assertNotNull(report);
+        Assert.assertEquals(1, report.getDependencies().size());
+        Assert.assertEquals(0, report.getUnresolvedDependencies().length);
+    }
+
+}
