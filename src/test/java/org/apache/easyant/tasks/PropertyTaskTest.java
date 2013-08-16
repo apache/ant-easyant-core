32317014095f5d9702c58f06cf6924c0335b6f33
Jean-Louis Boudart <jlboudart@apache.org>
2013-08-16 20:28:43 +0000

    Add property task tests
    git-svn-id: https://svn.apache.org/repos/asf/ant/easyant/core/trunk@1514883 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/java/org/apache/easyant/tasks/PropertyTaskTest.java
@@ -0,0 +1,77 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+package org.apache.easyant.tasks;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.junit.Assert.assertThat;
+
+import org.apache.easyant.core.EasyAntMagicNames;
+import org.apache.tools.ant.Project;
+import org.junit.Before;
+import org.junit.Test;
+
+public class PropertyTaskTest extends BaseAntTaskTest {
+
+    private PropertyTask propertyTask;
+
+    @Before
+    public void setUp() {
+        Project project = new Project();
+        configureProject(project);
+
+        propertyTask = new PropertyTask();
+        propertyTask.setProject(project);
+    }
+
+    @Test
+    public void shouldCreateProperty() {
+        propertyTask.setName("a-property");
+        propertyTask.setValue("a-value");
+        propertyTask.execute();
+
+        assertThat(propertyTask.getProject().getProperty("a-property"), equalTo("a-value"));
+    }
+
+    @Test
+    public void shouldNotCreatePropertyWhenConfigurationDoesntMatch() {
+        propertyTask.getProject().setProperty(EasyAntMagicNames.AVAILABLE_BUILD_CONFIGURATIONS, "aBuildConfNotActive");
+
+        propertyTask.setName("a-property");
+        propertyTask.setValue("a-value");
+        propertyTask.setConf("aBuildConfNotActive");
+        propertyTask.execute();
+
+        assertThat(propertyTask.getProject().getProperty("a-property"), is(nullValue()));
+    }
+
+    @Test
+    public void shouldCreatePropertyWhenConfigurationMatch() {
+        propertyTask.getProject().setProperty(EasyAntMagicNames.AVAILABLE_BUILD_CONFIGURATIONS, "aBuildConfActive");
+        propertyTask.getProject().setProperty(EasyAntMagicNames.MAIN_CONFS, "aBuildConfActive");
+
+        propertyTask.setName("a-property");
+        propertyTask.setValue("a-value");
+        propertyTask.setConf("aBuildConfActive");
+        propertyTask.execute();
+
+        assertThat(propertyTask.getProject().getProperty("a-property"), equalTo("a-value"));
+    }
+
+}
