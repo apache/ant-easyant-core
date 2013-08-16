91eefb01f8dd60b618472409d8fd9fee360a4d38
Jean-Louis Boudart <jlboudart@apache.org>
2013-08-16 20:28:14 +0000

    Add some helper class to tests log messages in tests
    git-svn-id: https://svn.apache.org/repos/asf/ant/easyant/core/trunk@1514882 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/java/org/apache/easyant/tasks/BaseAntTaskTest.java
@@ -0,0 +1,57 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+package org.apache.easyant.tasks;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.assertThat;
+
+import org.apache.tools.ant.Project;
+
+public class BaseAntTaskTest {
+
+    private AntTestListener antTestListener;
+
+    public Project configureProject(Project project) {
+        antTestListener = new AntTestListener(Project.MSG_INFO);
+        project.addBuildListener(antTestListener);
+        return project;
+    }
+
+    public void assertLogContaining(String substring) {
+        checkAntListener();
+        String realLog = antTestListener.getLog();
+        assertThat(realLog,
+                containsString("expecting log to contain \"" + substring + "\" log was \"" + realLog + "\""));
+    }
+
+    public void assertLogNotContaining(String substring) {
+        checkAntListener();
+        String realLog = antTestListener.getLog();
+        assertThat(realLog, not(containsString("expecting log to contain \"" + substring + "\" log was \"" + realLog
+                + "\"")));
+    }
+
+    private void checkAntListener() {
+        if (antTestListener == null) {
+            throw new IllegalStateException(
+                    "Project is not properly configure, please invoke configureProject method first");
+        }
+    }
+
+}
